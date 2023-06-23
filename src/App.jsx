cc9dd2e5c4b75da18e9ac9ac702919118ae5f706
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-06-23 08:37:02 +0300

    styled components fundamentals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-function App() {
+import GlobalStyles from './styles/GlobalStyles';
+import { StyledApp } from './StyledApp';
+import Heading from './ui/Heading';
+import Row from './ui/Row';
+import Input from './ui/Input';
+import Button from './ui/Button';
 
+function App() {
   return (
     <>
-      <div>Hello World</div>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>Hello World</Heading>
+            <Button
+              variation='danger'
+              size='medium'
+              onClick={() => alert('Chizaa')}
+            >
+              Chizaa
+            </Button>
+            <Button
+              variation='secondary'
+              size='small'
+              onClick={() => alert('Chizaa')}
+            >
+              Chizaa
+            </Button>
+          </Row>
+          <Row>
+            <Heading as='h3'>Form</Heading>
+            <form action=''>
+              <Input type='number' />
+              <Input type='type' />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
