09687c155bc904a6ad38759c68af0b4b8b634b19
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-06-26 10:30:04 +0300

    chore: setup react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import Dashboard from "./pages/Dashboard"
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
 import Settings from './pages/Settings';
 import Users from './pages/Users';
 import Login from './pages/Login';
-import PageNotFound from './pages/PageNotFound'
-import GlobalStyles from './styles/GlobalStyles'
-import AppLayout from "./ui/AppLayout";
+import PageNotFound from './pages/PageNotFound';
+import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
+
+
 
 
 function App() {
   return (
-    <>
+	<>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout/>}>
+          <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to='dashboard' />} />
             <Route path='dashboard' element={<Dashboard />} />
             <Route path='account' element={<Account />} />
@@ -34,4 +37,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
