9e172b0e4ff4d6777d04467f20b520a9684ad052
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-06-28 11:18:43 +0300

    feat: implement uploading image to storage bucket

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -12,32 +12,49 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  //https://jzvktezvkhcxowmjnswc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
 
-const { data, error } = await supabase
-  .from('cabins')
-  .insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
 
-   if (error) {
-     console.error(error);
-     throw new Error('Cabins could not get loaded');
-   }
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
-   return data;
+  //Create cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }]);
 
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not get loaded');
+  }
+
+  //upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error uplaoding image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error(
+      'Cabin image could not be uploaded and the cabin was not created'
+    );
+  }
+
+  return data;
 }
 
-export async function deleteCabin(id){
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
-const { data, error } = await supabase
-  .from('cabins')
-  .delete()
-  .eq('id', id);
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be deleted');
+  }
 
-   if (error) {
-     console.error(error);
-     throw new Error('Cabins could not be deleted');
-   }
-
-   return data;
-
+  return data;
 }
